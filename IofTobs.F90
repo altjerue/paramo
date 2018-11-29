de0fc7ee3811e43b89aca65dbee658086d89c3ae
jesus <jerue@fujiserver.daa.uv.es>
2018-11-29 21:40:19 +0100

    A way to fix data in HDF5 datasets

--- IofTobs.F90
+++ IofTobs.F90
@@ -56,6 +56,7 @@ program IofTobs
    call h5io_rdble0(group_id, 'redshift', z, herror)
    call h5io_rdble0(group_id, 'gamma_bulk', Gbulk, herror)
    call h5io_rdble0(group_id, 'view-angle', theta, herror)
+   call h5io_closeg(group_id, herror)
    call h5io_rint0(file_id, 't_stop', numdt, herror)
 
    allocate(t(numdt), nu(numdf), s(numdt), t_obs(numdt), pos(numdt))
@@ -75,10 +76,14 @@ program IofTobs
 
    Iobs = 0d0
    !!!! WARNING: This is just temporal
-   R = 1e7
-   call h5ldelete_f(group_id, 'R', herror)
-   call h5io_wdble0(group_id, 'R', R, herror)
-   call h5io_closeg(group_id, herror)
+   call h5ldelete_f(file_id, 'time', herror)
+   call h5ldelete_f(file_id, 't_obs', herror)
+   call h5ldelete_f(file_id, 'sen_lum', herror)
+   call h5ldelete_f(file_id, 'jnut', herror)
+   call h5io_wdble1(file_id, 'time', t(:numdt - 1), herror)
+   call h5io_wdble1(file_id, 't_obs', t_obs(:numdt - 1), herror)
+   call h5io_wdble1(file_id, 'sen_lum', s(:numdt - 1), herror)
+   call h5io_wdble2(file_id, 'jnut', jnut(:, :numdt - 1), herror)
    !!!!!!!!!!!!!!!!!!!!!!!!!!
    ! --> Light path from origin to the observer: 2 s mu_com
    ! --> Edge of the blob: 2 R mu_com
