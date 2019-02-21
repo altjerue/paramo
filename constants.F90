67dffcbc5d5efe3bd8ebd8ced9df54e12dd9e8be
Jesus Rueda <jruedabe@purdue.edu>
2019-02-20 19:13:07 -0500

    Debugging constants

--- constants.F90
+++ constants.F90
@@ -41,7 +41,7 @@ module constants
       nuconst = 2.7992491077281560779657886d6, &    ! eCharge / (2 * pi * m_e * cLight)
       jmbconst = 6.664569709819094d-30, &   ! sqrt(3) * eCharge**2 / (2 * cLight)
       ambconst = 3.6580794221267223d-3, &   ! sqrt(3) * eCharge**2 / (4 * m_e * cLight)
-      Bcritical = 4.414e13                  ! mass_e**2 * cLight**3 / (eCharge * hbar)
+      Bcritical = 4.414e13, &               ! mass_e**2 * cLight**3 / (eCharge * hbar)
       mec2_h = 1.235589965126603d20, &      ! m_e c^2 / h
       h_mec2 = 8.093299785722493d-21        ! h / m_e c^2
 
