5d03b4023ef1b728abb11ddd45b673406bb20693
Zach Davis <zkd@purdue.edu>
2020-02-21 15:36:24 -0500

    more sudo

--- zkdtests.F90
+++ zkdtests.F90
@@ -10,7 +10,10 @@
 
 !again not sure what implicit is
 !reserve memory for all needed variable and outputs? so learn how variable are made in fortran
+  !--outputs are managed by using the intent(out)
+  !--intent doesn't appear to be needed if it is not a parameter or variable that leaves
 !then initiallize all inputs taht are scalar
 !next initiallize all matrix and vectors
 !learn how for loops and if statements work in fortran
+  !--looks like you can name for loops adn if you do you end with end do NAME
 !end function and in theory feel proud
