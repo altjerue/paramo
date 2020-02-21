c240534a33fe2c0b1e7402e884a88bf7951479d4
Zach Davis <zkd@purdue.edu>
2020-02-21 15:07:42 -0500

    added some sudo code to help me step out the process

---
+++ zkdtests.F90
@@ -0,0 +1,16 @@
+!some sudo code
+!gram looks like a name to call the program by so make this.
+!add all dependencies
+!not sure what implicit is
+
+!add subroutine to do are calculation:
+!write finised ie learn how write function works
+
+!create the functrion you called earlier to do the calculation
+
+!again not sure what implicit is
+!reserve memory for all needed variable and outputs? so learn how variable are made in fortran
+!then initiallize all inputs taht are scalar
+!next initiallize all matrix and vectors
+!learn how for loops and if statements work in fortran
+!end function and in theory feel proud
