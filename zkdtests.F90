033b15d4e924fbb05369ca3c47f493b40760d742
Zach Davis <zkd@purdue.edu>
2020-02-24 16:20:11 -0500

    all distros

--- zkdtests.F90
+++ zkdtests.F90
@@ -31,9 +31,9 @@ contains
     implicit none
     integer(HID_T) :: file_id
     integer :: i, k, numg, numt, herror, l
-    real(dp) :: g1, g2, gmin, gmax, tacc, qind, R, tmax, tstep
+    real(dp) :: g1, g2, gmin, gmax, tacc, qind, R, tmax, tstep, DynT, tesc, tescdyn
     real(dp), allocatable, dimension(:) :: t, g, dt, C0,D0, Q0, zero1, zero2,Diff
-    real(dp), allocatable, dimension(:, :) :: n1
+    real(dp), allocatable, dimension(:, :) :: n1,n2,n3
     !then initiallize all inputs taht are scalar
     !next initiallize all matrix and vectors
     numg=128
@@ -48,7 +48,7 @@ contains
     R = 1e16
 
     allocate(g(numg),t(0:numt),dt(numt),Diff(numt),D0(numg), Q0(numg),C0(numg), zero1(numg), zero2(numg))
-    allocate(n1(0:numt, numg))
+    allocate(n1(0:numt, numg),n2(0:numt, numg),n3(0:numt, numg))
 
     !-- not sure what build_g is doing
     build_g: do k = 1, numg
@@ -63,21 +63,24 @@ contains
     zero2 = 0d0
     C0 = 3.48d-11 ! 4d0 * sigmaT * uB / (3d0 * mass_e * cLight)
     tacc = 1d0 / (C0(1) * 10d0**4.5d0) !tesc
+    tesc=tacc
     D0 = 0.5d0 * pofg(g)**2 / tacc
     n1(0, :) = injection_pwl(1d0, tacc, g, g1, g2, qind, 1d0)
-
+    dynT=R/cLight
 
     !D_t(0)=D0
     time_loop: do i = 1, numt
 
       t(i) = tstep * ( (tmax / tstep)**(dble(i - 1) / dble(numt - 1)) )
       dt(i) = t(i) - t(i - 1)
-
-      Diff = D0*t(i)/tmax
+      tescdyn=tesc*t(i)/dynT
+      Diff = D0*t(i)/dynT
       !Q0 = injection_pwl(t(i), tacc, g, g1, g2, qind, 1d0)
       !--- learn how for loops and if statements work in fortran
       !--looks like you can name for loops adn if you do you end with end do NAME
       call FP_FinDif_difu(dt(i), g, n1(i - 1, :), n1(i, :), C0 * pofg(g)**2, Diff, zero2, 1d200, R / cLight)
+      call FP_FinDif_difu(dt(i), g, n2(i - 1, :), n2(i, :), C0 * pofg(g)**2, zero1, zero2, tescdyn, R / cLight)
+      call FP_FinDif_difu(dt(i), g, n3(i - 1, :), n3(i, :), C0 * pofg(g)**2, Diff, zero2, tescdyn, R / cLight)
 
     end do time_loop
 
@@ -86,6 +89,8 @@ contains
     call h5io_wdble1(file_id, 'time', t(1:), herror)
     call h5io_wdble1(file_id, 'gamma', g, herror)
     call h5io_wdble2(file_id, 'dist1', n1(1:, :), herror)
+    call h5io_wdble2(file_id, 'dist2', n2(1:, :), herror)
+    call h5io_wdble2(file_id, 'dist3', n3(1:, :), herror)
     call h5io_closef(file_id, herror)
     call h5close_f(herror)
 
