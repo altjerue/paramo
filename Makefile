33c6a5ce2186695816fd18e1f69ecb425d4b6a14
Zach Davis <zkd@purdue.edu>
2020-03-02 02:49:29 -0500

    an attempt at recreating turbulence paper distros. diffusion parameter is way off

--- Makefile
+++ Makefile
@@ -1,19 +1,20 @@
 # definitions Is this a compiler?
-FC=h5fc
+FC=h5fc #if not using h5c this would be gfortran
 # -----  executables  -----this creates the files names for the eventual compiled programs
 
 TESTS=xTestszkd
 
 
 # -----  dependencies  ----- gets everything from use in the F90.slash represents inputs and output files. idk why hdf5 isnt here maybe it apart of fortran
-TESTS_OBJ = misc.o params.o pwl_integ.o K2.o SRtoolkit.o h5_inout.o \
-	dist_evol.o zkdtests.o
+TESTS_OBJ = data_types.o h5_inout.o dist_evol.o zkdtests.o constants.o misc.o \
+	pwl_integ.o SRtoolkit.o K2.o
 # -----  rules  ----- im not sure what this is yet ... this might be way of introducing short hand such as make all
 
 # objects this seems to further break down dependencies for other scripts you have
 dist_evol.o: data_types.o constants.o misc.o pwl_integ.o SRtoolkit.o K2.o
 # executables creating the programs from above
 $(TESTS): $(TESTS_OBJ)
+	echo "compiling $@ with these objects: $^"
 	$(FC) -o $@ $^
 
 
@@ -22,7 +23,7 @@ $(TESTS): $(TESTS_OBJ)
 #not sure what this is jesus $(LOPT)
 
 %.o: %.F90 #maybe all .0 outputs come from a corresponding .F90 input?
-	$(FC) $(COPT) $< -o $@
+	$(FC) -c $< -o $@
 
 
 
--- zkdtests.F90
+++ zkdtests.F90
@@ -24,7 +24,7 @@ contains
     implicit none
     integer(HID_T) :: file_id
     integer :: i, k, numg, numt, herror, l
-    real(dp) :: g1, g2, gmin, gmax, tacc, qind, R, tmax, tstep, DynT, tesc
+    real(dp) :: g1, g2, gmin, gmax, tacc, qind, R, tmax, tstep, DynT, tesc, th
     real(dp), allocatable, dimension(:) :: t, g, dt, C0,D0, Q0, zero1, zero2,Diff
     real(dp), allocatable, dimension(:, :) :: n1,n2,n3
     !then initiallize all inputs taht are scalar
@@ -34,16 +34,16 @@ contains
 
     open(2,file='input.txt')
 
-    read(2,*) numg
-    read(2,*) numt
-    read(2,*) qind
-    read(2,*) tstep
-    read(2,*) tmax
-    read(2,*) g1
-    read(2,*) g2
-    read(2,*) gmin
-    read(2,*) gmax
-    read(2,*) R
+    numg=128
+    numt =300
+    qind=0d0
+    tstep=1e0
+    tmax=1e8
+    g1=1e1
+    g2=1e6
+    gmin=1.01d0
+    gmax =1.5d0 * g2
+    R=1e16
 
     close(2)
     write(*,*) numg,numt,qind,tstep,tmax,g1,g2,gmin,gmax,R
@@ -67,9 +67,10 @@ contains
     tacc = 1d0 / (C0(1) * 10d0**4.5d0) !tesc
     tesc=tacc
     D0 = 0.5d0 * pofg(g)**2 / tacc
-    n1(0, :) = injection_pwl(1d0, tacc, g, g1, g2, qind, 1d0)
-    n2(0, :) = n1(0, :)
-    n3(0, :) =n1(0, :)
+    th = 1d2
+    n1(0, :) = RMaxwell_v(g,th)
+    !n2(0, :) = n1(0, :)
+    !n3(0, :) =n1(0, :)
     dynT=R/cLight
 
     !D_t(0)=D0
@@ -77,14 +78,14 @@ contains
 
       t(i) = tstep * ( (tmax / tstep)**(dble(i - 1) / dble(numt - 1)) )
       dt(i) = t(i) - t(i - 1)
-      Diff = D0*t(i)/dynT
+      Diff = 1.8*pofg(g)**2 + 1.8*((3d2)**2)
       !Q0 = injection_pwl(t(i), tacc, g, g1, g2, qind, 1d0)
       !--- learn how for loops and if statements work in fortran
       !--looks like you can name for loops adn if you do you end with end do NAME
-      call FP_FinDif_difu(dt(i), g, n1(i - 1, :), n1(i, :), C0 * pofg(g)**2, Diff, zero2, 1d200, R / cLight)
-      call FP_FinDif_difu(dt(i), g, n2(i - 1, :), n2(i, :), (t(i)/dynT)*C0 * pofg(g)**2, zero1, zero2, 1d200, R / cLight)
-      call FP_FinDif_difu(dt(i), g, n3(i - 1, :), n3(i, :), (t(i)/dynT)*C0 * pofg(g)**2, Diff, zero2, 1d200, R / cLight)
-      write(*,*) n3(i,100)
+      call FP_FinDif_difu(dt(i), g, n1(i - 1, :), n1(i, :), C0 * pofg(g)**2, Diff, zero2, 1d200, R / cLight)!what is r/clight???  tlc was added since old
+      !call FP_FinDif_difu(dt(i), gamma, distroin, distrout, gammadot, diffusion, Injection, escape, R / cLight)
+      !call FP_FinDif_difu(dt(i), g, n2(i - 1, :), n2(i, :), (t(i)/dynT)*C0 * pofg(g)**2, zero1, zero2, 1d200, R / cLight)
+      !call FP_FinDif_difu(dt(i), g, n3(i - 1, :), n3(i, :), (t(i)/dynT)*C0 * pofg(g)**2, Diff, zero2, 1d200, R / cLight)
     end do time_loop
 
     call h5open_f(herror)
@@ -92,17 +93,11 @@ contains
     call h5io_wdble1(file_id, 'time', t(1:), herror)
     call h5io_wdble1(file_id, 'gamma', g, herror)
     call h5io_wdble2(file_id, 'dist1', n1(1:, :), herror)
-    call h5io_wdble2(file_id, 'dist2', n2(1:, :), herror)
-    call h5io_wdble2(file_id, 'dist3', n3(1:, :), herror)
+  !  call h5io_wdble2(file_id, 'dist2', n2(1:, :), herror)
+  !  call h5io_wdble2(file_id, 'dist3', n3(1:, :), herror)
     call h5io_closef(file_id, herror)
     call h5close_f(herror)
 
-    !just as an example I am going to write time to a file_id
-    open(1, file = 'testdata.txt')
-    do i=1, numt
-      write(1,*) t(i)
-    end do
-    close(1)
 
 
     !end function and in theory feel proud
